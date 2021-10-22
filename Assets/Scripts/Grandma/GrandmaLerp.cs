519b22eee959b53b08dbaf8c1fb23acbdc7e05dd
Dimitri <dimitri@formitize.com>
2021-10-22 18:03:31 +1100

    few updates

--- Assets/Scripts/Grandma/GrandmaLerp.cs
+++ Assets/Scripts/Grandma/GrandmaLerp.cs
@@ -6,6 +6,8 @@ public class GrandmaLerp : MonoBehaviour
 {
     [SerializeField] SharedInt playerHealth;
     [SerializeField] GameObject target;
+    [SerializeField] GameObject bringHimToMe;
+    private float startCameraDollyYPos = 0;
 
     [SerializeField] Animator animator;
     [SerializeField] Animator kidAnimator;
@@ -15,6 +17,7 @@ public class GrandmaLerp : MonoBehaviour
     void Start()
     {
         destXPos = startXPos = this.transform.position.x;
+        startCameraDollyYPos = bringHimToMe.transform.localPosition.y; 
         playerHealth.Callback += CalculateNewPosition;
     }
 
@@ -24,9 +27,19 @@ public class GrandmaLerp : MonoBehaviour
         var percentage = ((float)value/100f);
 
         destXPos = Mathf.Lerp(
-            target.transform.position.x,
+            target.transform.position.x + 2,
             startXPos,
             percentage
+        );
+
+        bringHimToMe.transform.localPosition = new Vector3(
+            0,
+            Mathf.Lerp(
+                (transform.position.y + 10),
+                startCameraDollyYPos,
+                percentage
+            ),
+            0
         );
 
         direction = destXPos <= this.transform.position.x ? -1 : 1;
